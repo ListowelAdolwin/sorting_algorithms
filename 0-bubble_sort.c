7e282bd86f43ee912332e756e96945bb0bb43c33
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-13 12:49:39 +0000

    Sort an array with bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+
+/**
+ * bubble_sort- sorts an arraiy in ascending order
+ * @array: the array to be sorted
+ * @size: the size of the array
+ * return: void
+ *
+ * print_array - prints the array after every swap
+ *
+ */
+
+
+void bubble_sort(int *array, size_t size)
+{
+	int pass = 0, comp = 0, tmp = 0;
+
+
+	if (size < 2)
+	{
+		print_array(array, size);
+	}
+
+	else
+	{
+		for (pass = 0; pass < size - 1; pass++)
+		{
+			for (comp = 0; comp < size - 1 - pass; comp++)
+			{
+				if (array[comp] > array[comp + 1])
+				{
+					tmp = array[comp];
+					array[comp] = array[comp + 1];
+					array[comp + 1] = tmp;
+					print_array(array, size);
+				}
+			}
+		}
+	}
+}
