c68154650d666ba7fc829eed34ade411d5a13680
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-13 10:57:04 +0000

    Added header file

---
+++ sort.h
@@ -0,0 +1,15 @@
+#ifndef
+#define SORT_H
+
+/**
+ * description- Header file containing prototypes of the sorting of
+ * the sorting algorithm project
+ */
+ 
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+#endif /* SORT_H */
