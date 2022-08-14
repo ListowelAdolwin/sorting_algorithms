73b7f1f39179d66adb2af6390d194abef8e5bb21
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-14 23:29:20 +0000

    Updated readme file

--- sort.h
+++ sort.h
@@ -1,10 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
-/**
- * description- Header file containing prototypes of the sorting of
- * the sorting algorithm project
- */
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -13,6 +11,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
 	const int n;
@@ -20,10 +19,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void selection_sort(int *array, size_t size);
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-
-#endif /* SORT_H */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void swapper(listint_t *a, listint_t *b);
+void swaps(int *array, size_t size, int *a, int *b);
+void kwiksort(int *array, size_t size, ssize_t low, ssize_t high);
+size_t partition(int *array, size_t size, ssize_t low, ssize_t high);
+#endif
