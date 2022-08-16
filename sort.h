4ec32e24fb0d3526830432d78cd9b1dffcf1fd73
ListowelAdolwin <listoweladolwin@gmail.com>
2022-08-16 13:35:56 +0000

    Updated header file

--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void quickSort(int arr[], int low, int high, size_t size);
